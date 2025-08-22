7eb2f7cedfd5ed3252b027a123f8fd4b69eb390b
Alex | Kronox <alexander_vogt@outlook.de>
2025-08-22 16:42:14 +0200

    crude mobile support

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="grid h-screen min-h-screen w-screen grid-cols-[3fr_1fr] grid-rows-[1fr_auto] gap-x-5 gap-y-3 bg-slate-700 p-5 pb-2 md:overflow-hidden"
+    class="grid md:h-screen min-h-screen w-screen grid-cols-1 md:grid-cols-[3fr_1fr] md:grid-rows-[1fr_auto] gap-x-5 gap-y-3 bg-slate-700 p-5 pb-2 md:overflow-hidden"
   >
-    <SelectorContainer class="col-start-1 row-start-1 min-h-full w-full" />
-    <div class="flex flex-col gap-5 md:max-h-full md:overflow-hidden">
+    <SelectorContainer class="col-start-1 row-start-1 md:min-h-full w-full" />
+    <div class="row-start-2 md:row-start-1 flex flex-col gap-5 md:max-h-full md:overflow-hidden">
       <FishContainer class="w-fill top col-start-2" :fish="store().results"></FishContainer>
       <ChestContainer class="w-fill bottom h-fit max-h-1/2" :fish="store().results" />
     </div>
-    <div class="col-span-2 row-start-2 text-xs text-white">
+    <div class="md:col-span-2 row-start-3 md:row-start-2 text-xs text-white">
       Writen by <a href="https://www.github.com/Kr0nox">Kronox</a> This tool is heavily inspired by
       <a href="https://brokencygnus.github.io/stardew-fishing-calc/home"
         >brokencygnus Fishing Calculator</a
@@ -36,11 +36,14 @@ a:visited {
   @apply text-purple-500;
 }
 
+@media (width >= 48rem) {
 .top {
   flex: 1 1 auto;
 }
 
 .bottom {
   flex: 0 0 auto;
+
+}
 }
 </style>
--- src/components/selectors/SelectorContainer.vue
+++ src/components/selectors/SelectorContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <ContainerComponent class="bg-slate-100 p-2!">
     <div
-      class="grid grid-cols-[1fr_1fr_2fr] grid-rows-[auto_1fr] gap-3 md:max-h-full md:overflow-scroll"
+      class="grid grid-cols-1 md:grid-cols-[1fr_1fr_2fr] md:grid-rows-[auto_1fr] gap-3 md:max-h-full md:overflow-scroll"
     >
       <div class="col-start-1 row-start-1 flex flex-col gap-3">
         <SeasonSelector />
@@ -10,14 +10,14 @@
 
         <SaveManager />
       </div>
-      <div class="col-start-2 row-start-1 flex flex-col gap-3">
+      <div class="md:col-start-2 md:row-start-1 flex flex-col gap-3">
         <BaitSelector />
         <ModifierOptions />
         <SliderOptions />
       </div>
-      <div class="col-span-2 col-start-1 row-start-2 flex flex-col gap-3"></div>
+      <div class="md:col-span-2 md:col-start-1 md:row-start-2 flex flex-col gap-3"></div>
 
-      <div class="col-start-3 row-span-2 row-start-1 flex flex-col gap-3">
+      <div class="md:col-start-3 md:row-span-2 md:row-start-1 flex flex-col gap-3">
         <LocationSelector />
         <TimeSelector />
       </div>
