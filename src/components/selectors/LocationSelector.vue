07538ce24fb7ce349b321f1f1e309048dc3c0e33
Alexander Vogt <alexander_vogt@outlook.de>
2025-08-31 13:52:43 +0200

    move locations so no gap is present

--- src/components/selectors/LocationSelector.vue
+++ src/components/selectors/LocationSelector.vue
@@ -2,13 +2,17 @@
   <ContainerComponent class="border-orange-400! bg-orange-200 p-2!">
     <div class="grid grid-cols-1 gap-2 md:grid-cols-2">
       <ContainerComponent
-        v-for="location in LocationOptions"
+        v-for="(location, index) in LocationOptions"
         :key="location.name"
         :class="
           store().location.location == location.name
-            ? 'col-start-1 border-orange-600! bg-orange-400 md:col-span-2'
+            ? 'border-orange-600! bg-orange-400 md:col-span-2'
             : 'bg-slate-100'
         "
+        :style="{
+          'grid-column-start': getColumn(index),
+          'grid-row-start': getRow(index)
+        }"
         class="cursor-pointer"
         @click="selectLocation(location)"
       >
@@ -17,7 +21,10 @@
             <img :src="LocationImages[location.name]" class="h-4" />
             {{ location.name }}
           </span>
-          <div v-if="store().location.location == location.name" class="flex flex-col gap-y-2">
+          <div
+            v-if="store().location.location == location.name && location.subLocations.length > 0"
+            class="flex flex-col gap-y-2"
+          >
             <ContainerComponent
               v-for="subLocation in location.subLocations"
               :key="subLocation"
@@ -43,6 +50,7 @@ import { LocationOptions, type LocationOption } from '@/model/location'
 import ContainerComponent from '../ContainerComponent.vue'
 import { store } from '@/store'
 import { LocationImages } from '@/model/images'
+import { computed } from 'vue'
 
 function selectLocation(location: LocationOption) {
   if (location.name == store().location.location) return
@@ -54,5 +62,28 @@ function setSubLocation(subLocation: string, e: Event) {
   e.preventDefault()
   e.stopPropagation()
   store().location.subLocation = subLocation
+}
+
+const selectedIndex = computed(() =>
+  LocationOptions.findIndex((loc) => loc.name == store().location.location)
+)
+const selectedModulo = computed(() => selectedIndex.value % 2)
+
+function getColumn(index: number) {
+  if (index == selectedIndex.value) return 1
+  if (index < selectedIndex.value) return (index % 2) + 1
+  if (index == selectedIndex.value + 1 && selectedModulo.value == 1) return 2
+  if (selectedModulo.value == 0) return ((index - 1) % 2) + 1
+  return (index % 2) + 1
+}
+
+function getRow(index: number) {
+  const defaultRow = Math.floor(index / 2) + 1
+  if (index < selectedIndex.value) return defaultRow
+  if (index == selectedIndex.value) {
+    return selectedModulo.value == 0 ? defaultRow : defaultRow + 1
+  }
+  if (index == selectedIndex.value + 1 && selectedModulo.value == 1) return defaultRow - 1
+  return defaultRow + (index % 2 == 0 ? 0 : 1)
 }
 </script>
